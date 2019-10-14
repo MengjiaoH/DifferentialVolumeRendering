036862094c6e6ea80527212250ddbb2272147a17
hanmj <mengjiao@sci.utah.edu>
2019-10-14 14:25:37 -0600

    link libs correctly

--- dataType.h
+++ dataType.h
@@ -6,7 +6,6 @@
 #include "ospcommon/math/vec.h"
 #include "ospcommon/math/box.h"
 
-#include "PIDX.h"
 
 struct timesteps
 {
@@ -41,40 +40,4 @@ struct IDXVar {
     std::string type;
 };
 
-struct PIDXVolume {
-    std::string datasetPath;
-    PIDX_access pidxAccess;
-    PIDX_file pidxFile;
-    PIDX_file outpidxFile;
-    PIDX_metadata_cache cache;
-    PIDX_point pdims;
-    std::vector<std::string> pidxVars;
 
-    PIDX_variable var;
-    int resolution;
-    vec3sz fullDims, localDims, localOffset;
-    ospcommon::math::box3f localRegion;
-    ospcommon::math::vec2f valueRange;
-    std::vector<char> data; 
-    int valuesPerSample = 0;
-    int bitsPerSample = 0;
-    std::string type;
-
-    // UI data
-    std::string currentVariableName;
-    int currentVariable;
-    size_t currentTimestep;
-                            
-    PIDXVolume(const std::string &path, 
-               const std::string &currentVariableName, 
-               size_t currentTimestep);
-    
-    PIDXVolume(const PIDXVolume &p) = delete;
-    PIDXVolume& operator=(const PIDXVolume &p) = delete;
-    ~PIDXVolume();
-                            
-    private:
-        void update();
-        void set_pidx_file(int ts);
-        void set_pidx_variable();
-};
--- loadPIDX.h
+++ loadPIDX.h
@@ -4,6 +4,8 @@
 #include <vector>
 #include <chrono>
 
+#include "PIDX.h"
+
 #include "dataType.h"
 #include "pidxUtils.h"
 
@@ -11,3 +13,40 @@ IDXVar parse_idx_type(const std::string &type);
 
 ospcommon::math::vec3i computeGrid(int num);
 
+struct PIDXVolume {
+    std::string datasetPath;
+    PIDX_access pidxAccess;
+    PIDX_file pidxFile;
+    PIDX_file outpidxFile;
+    PIDX_metadata_cache cache;
+    PIDX_point pdims;
+    std::vector<std::string> pidxVars;
+
+    PIDX_variable var;
+    int resolution;
+    vec3sz fullDims, localDims, localOffset;
+    ospcommon::math::box3f localRegion;
+    ospcommon::math::vec2f valueRange;
+    std::vector<char> data; 
+    int valuesPerSample = 0;
+    int bitsPerSample = 0;
+    std::string type;
+
+    // UI data
+    std::string currentVariableName;
+    int currentVariable;
+    size_t currentTimestep;
+                            
+    PIDXVolume(const std::string &path, 
+               const std::string &currentVariableName, 
+               size_t currentTimestep);
+    
+    PIDXVolume(const PIDXVolume &p) = delete;
+    PIDXVolume& operator=(const PIDXVolume &p) = delete;
+    ~PIDXVolume();
+                            
+    private:
+        void update();
+        void set_pidx_file(int ts);
+        void set_pidx_variable();
+};
